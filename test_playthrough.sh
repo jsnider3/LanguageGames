c3281ed8a9d30cb7a407dcfef3d479834f041bff
Joshua Snider <josh@joshuasnider.com>
2025-07-04 02:01:01 -0400

    Test: Add automated playthrough script

---
+++ test_playthrough.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+python3 game.py <<EOF
+look
+examine desk
+read note
+inv
+examine lord alistair
+go lounge
+go ballroom
+go gardens
+inspect rose bushes
+examine locket
+inventory
+go ballroom
+go lounge
+talk to lady eleanor
+accuse lady eleanor
+EOF
